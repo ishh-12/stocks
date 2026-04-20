a49848e2bd35fc58da2c8211fc3b059768158162
Your Name <your-email@example.com>
2026-04-20 15:15:04 +0530

    Fix CORS, deployment URLs, and mobile awards UI

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -5,19 +5,23 @@ function Awards() {
   const pressLogosImage = `${process.env.PUBLIC_URL}/media/images/pressLogos.png`;
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-5 awards-section">
       <div className="row">
-        <div className="col-6 p-5">
-          <img src={largestBrokerImage} alt="Largest stock broker in India" />
+        <div className="col-12 col-md-6 p-4 p-md-5 awards-image-col">
+          <img
+            src={largestBrokerImage}
+            alt="Largest stock broker in India"
+            className="awards-main-image"
+          />
         </div>
-        <div className="col-6 p-5 mt-5">
+        <div className="col-12 col-md-6 p-4 p-md-5 mt-0 mt-md-5 awards-content-col">
           <h1>Largest stock broker in India</h1>
           <p className="mb-5">
             2+ million Zerodha clients contribute to over 15% of all retail
             order volumes in India daily by trading and investing in:
           </p>
           <div className="row">
-            <div className="col-6">
+            <div className="col-12 col-sm-6">
               <ul>
                 <li>
                   <p>Futures and Options</p>
@@ -30,7 +34,7 @@ function Awards() {
                 </li>
               </ul>
             </div>
-            <div className="col-6">
+            <div className="col-12 col-sm-6">
               <ul>
                 <li>
                   <p>Stocks & IPOs</p>
@@ -44,7 +48,7 @@ function Awards() {
               </ul>
             </div>
           </div>
-          <img src={pressLogosImage} style={{ width: "90%" }} alt="Press logos" />
+          <img src={pressLogosImage} className="awards-press-logos" alt="Press logos" />
         </div>
       </div>
     </div>
